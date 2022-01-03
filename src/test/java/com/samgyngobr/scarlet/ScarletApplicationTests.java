45e514aff59d9055b2d850484b79a21ba586f047
Sam <sam@localhost.localdomain>
2022-01-03 01:55:08 -0300

    Initial commit

---
+++ src/test/java/com/samgyngobr/scarlet/ScarletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samgyngobr.scarlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScarletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
