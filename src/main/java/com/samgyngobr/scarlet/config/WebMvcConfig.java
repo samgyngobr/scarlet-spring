c9619c886228187eeddc82d5a15ff3dfefd05e8a
Samuel J. Oliveira <52247571+samgyngobr@users.noreply.github.com>
2022-01-13 17:52:09 -0300

    test

---
+++ src/main/java/com/samgyngobr/scarlet/config/WebMvcConfig.java
@@ -0,0 +1,39 @@
+package com.samgyngobr.scarlet.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.samgyngobr.scarlet.spring"})
+public class WebMvcConfig implements WebMvcConfigurer
+{
+    @Bean
+    public InternalResourceViewResolver resolver()
+    {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+
+    @Bean
+    public MessageSource messageSource()
+    {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages");
+
+        return source;
+    }
+
+}
--- src/main/java/com/samgyngobr/scarlet/controller/IndexController.java
+++ src/main/java/com/samgyngobr/scarlet/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.samgyngobr.scarlet.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class IndexController
@@ -14,4 +16,5 @@ public class IndexController
 
         return "index";
     }
+
 }
